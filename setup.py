6dc592c824a14f3a95a9cd7d3ccc61551edda989
Neha Sontakke <nsontakke004@gmail.com>
2026-03-11 13:55:52 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,33 @@
 from setuptools import setup, find_packages
 
 setup(
-  name="BLIMMP",
-  version="0.1.0",
-  author="Neha Sontakke",
-  license="MIT",
-  packages=find_packages(),
-  install_requires=[
-    "pandas>=1.5",
-    "numpy>=1.23"
-  ],
-  entry_points={
-    "console_scripts": [
-      "BLIMMP=BLIMMP_Scripts.module_detection:main",
+    name="BLIMMP",
+    version="0.1.0",
+    author="Neha Sontakke",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "pandas>=1.5",
+        "numpy>=1.23",
+        "numba",
+        "scipy",
     ],
-  },
+    package_data={
+        "BLIMMP_Scripts": [
+            "Graph_Dependencies/KEGG_Module_Graphs.zip",
+            "Graph_Dependencies/KEGG_Module_Equations_Jan26.json",
+            "Graph_Dependencies/module_ko_reaction.json",
+            "Graph_Dependencies/MODULE_ALL_NEIGHBOR_DATA/*",
+            "Data_Dependencies/ATB_Taxonomy_Frequency/*",
+            "Data_Dependencies/ko_list.txt",
+            "Data_Dependencies/module_freq.txt",
+        ]
+    },
+    include_package_data=True,
+    python_requires=">=3.10",
+    entry_points={
+        "console_scripts": [
+            "BLIMMP=BLIMMP_Scripts.module_detection:main",
+        ],
+    },
 )
-
