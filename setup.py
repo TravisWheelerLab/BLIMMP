3102942948c27c90ee9667e0cc7869203b7d6840
Travis <twheeler@arizona.edu>
2026-08-13 20:46:01 -0700

    Bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="BLIMMP",
-    version="0.1.5",
+    version="0.1.6",
     author="Neha Sontakke",
     license="MIT",
     packages=find_packages(),
